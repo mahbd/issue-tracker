5b509d079a1c0f10e65d07d500367d12dd643db0
Mahmudul Alam <mahmudula2000@gmail.com>
2023-10-23 16:46:16 +0600

    build an api for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -27,3 +27,20 @@ export async function PATCH(request: NextRequest, {params}: {params: {id: string
     });
     return NextResponse.json(updatedIssue);
 }
+
+export async function DELETE(request: NextRequest, {params}: {params: {id: string}}) {
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: Number(params.id)
+        }
+    });
+    if (!issue) {
+        return NextResponse.json({message: 'Issue not found'}, {status: 404});
+    }
+    await prisma.issue.delete({
+        where: {
+            id: issue.id
+        }
+    });
+    return NextResponse.json({message: 'Issue deleted'}, {status: 204});
+}
