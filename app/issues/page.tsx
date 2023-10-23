d52718586516e415f1db2f0fb00af1ba35518a42
Mahmudul Alam <mahmudula2000@gmail.com>
2023-10-23 11:23:54 +0600

    replace link in issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Button, Table, TextField } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../component/IssueStatusBadge";
 import delay from "delay";
 import IssueActions from "./IssueActions";
+import Link from "../component/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
   return (
     <div>
       <IssueActions />
