a4851dbc9ee065c8f7912d3edefe8b83bcd64b51
Mahmudul Alam <mahmudula2000@gmail.com>
2023-10-23 12:20:15 +0600

    build an api for updating issue

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,29 @@
+import { issueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(request: NextRequest, {params}: {params: {id: string}}) {
+    const body = await request.json();
+    const validation = issueSchema.safeParse(body);
+    if (!validation.success) {
+        return NextResponse.json(validation.error.format(), {status: 400});
+    }
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: Number(params.id)
+        }
+    });
+    if (!issue) {
+        return NextResponse.json({message: 'Issue not found'}, {status: 404});
+    }
+    const  updatedIssue = await prisma.issue.update({
+        where: {
+            id: Number(params.id)
+        },
+        data: {
+            title: body.title,
+            description: body.description
+        }
+    });
+    return NextResponse.json(updatedIssue);
+}
--- app/issues/IssueForm.tsx
+++ app/issues/IssueForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Spinner, ErrorMessage } from "@/app/components";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Issue } from "@prisma/client";
 import { Button, Callout, TextField } from "@radix-ui/themes";
@@ -17,7 +17,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 interface Props {
   issue?: Issue;
@@ -30,7 +30,7 @@ const IssueForm = ({ issue }: Props) => {
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
   const [error, setError] = useState("");
   const router = useRouter();
